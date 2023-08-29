c74a6265e926b6b32a7f19a001a3856f985976a0
Hyemin0102 <128768462+Hyemin0102@users.noreply.github.com>
2023-08-29 18:16:27 +0900

    카테고리 선백 css 수정, 이미지추가

--- src/Components/CategoryButton.tsx
+++ src/Components/CategoryButton.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { Category } from "../Api/HomePage";
+import { Category } from "./HomePage";
 
 
 type CategoryButtonProps = {
   category: Category,
   //onClick 추가, void 사용 => 인덱스를 받지만 return값은 없음 의미
-  onClick: (index:number) => void
+  onClick: (index:number) => void,
+  isSelected: boolean
 }
 
 
 
-export const CategoryButton = ({category, onClick}:CategoryButtonProps) =>{
-  const {id, lable} = category;
+export const CategoryButton = ({category, onClick, isSelected}:CategoryButtonProps) =>{
+  const {id, lable,image} = category;
 
   return(
-    <div className="space-x-4 
-    border-solid border border-blue-500" onClick={()=>onClick(id)}> {/* 클릭 시 카테고리의 id보냄 */}
-      <div className="">
-        <img src="https://image.tmdb.org/t/p/original/1YYL1OcgjPLjAGi6n0iZe1gdl9i.jpg" alt="이미지" 
+    <div className={`${isSelected ? 'font-bold' : 'opacity-50'} `} 
+      onClick={()=>onClick(id)}> {/* 클릭 시 카테고리의 id보냄 */}
+      <img src={image} alt="이미지" 
         className="rounded-full w-28 h-28 object-cover"/>
-      </div>
       <div className="text-center">{lable}</div>
     </div>
   )
--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Movie } from "../Api/HomePage";
+import { Movie } from "./HomePage";
 
 type MovieCardProps = {
   movie: Movie
